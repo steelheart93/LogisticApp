40f8b6a323b7a01ecab2087014fa42e2d149aa0d
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2019-08-14 22:37:26 -0500

    más cambios en EntradaLote

--- LogisticApp/Entidades/EntradaLote.cs
+++ LogisticApp/Entidades/EntradaLote.cs
@@ -7,19 +7,24 @@ namespace LogisticApp.Entidades
 {
     public class EntradaLote
     {
-        private Usuario registrador { get; set; }
-        private string codigo { get; set; }
-        private int cantidadInicial { get; set; }
-        private int cantidadActual { get; set; }
-        private string origen { get; set; }
-        private DateTime fechaExpiracion { get; set; }
-        private DateTime fechaHoraRegistro { get; set; }
-        private string ubicacion { get; set; }
-        private string observaciones { get; set; }
+        public Usuario registrador { get; private set; }
+        public string codigo { get; private set; }
+        public int cantidadInicial { get; private set; }
+        public int cantidadActual { get; private set; }
+        public string origen { get; private set; }
+        public DateTime fechaExpiracion { get; private set; }
+        public DateTime fechaHoraRegistro { get; private set; }
+        public string ubicacion { get; private set; }
+        public string observaciones { get; private set; }
 
         public void decrementarCantidadActual(int cantidad)
         {
             cantidadActual -= cantidad;
+        }
+
+        private EntradaLote()
+        {
+            Console.WriteLine("Hola soy el constructor privado de EntradaLote, ¿Por alguna razón?");
         }
     }
 }
