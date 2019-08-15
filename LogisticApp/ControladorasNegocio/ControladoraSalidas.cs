d3168138508fb9bd21af60ad83fcbd910cd738d7
Juan Sánchez <juan47s@hotmail.com>
2019-08-15 18:56:34 -0500

    pablo version 1

---
+++ LogisticApp/ControladorasNegocio/ControladoraSalidas.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LogisticApp.Entidades;
+
+namespace LogisticApp.ControladorasNegocio
+{
+    [Route("api/controladorasNegocio")]
+    [ApiController]
+    public class ControladoraSalidas : ControllerBase
+    {
+        private readonly <dbcontext> _context;
+
+        public ControladoraSalidas(<dbcontext> context)
+        {
+            _context = context;
+        }
+
+        public JsonResult getSalidas()
+        {
+            return _context.SalidaExistencias.ToList();
+        }
+
+        public JsonResult filtrarSalidas()
+        {
+            return _context.SalidaExistencias.find();
+        }
+
+        public void addSalida(string codigoProducto, SalidaExistencia salida)
+        {
+            _context.Productos.addSalidaExistencia(salida);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ LogisticApp/Entidades/SalidaExistencia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogisticApp.Entidades
+{
+    public class SalidaExistencia
+    {
+        public string codigo { get; }
+        public DateTime fechaHoraRegistro { get; }
+        public string destino { get; }
+        public string observaciones { get; }
+
+        public Dictionary<EntradaLote, int> lotesSalida { get; }
+        public Usuario registrador { get; }
+
+        public int getCantidadSalida()
+        {
+            int cantidadSalida = 0;
+            foreach (KeyValuePair<EntradaLote, int> kvp in lotesSalida)
+            {
+                cantidadSalida += kvp.Value;
+            }
+            return cantidadSalida;
+        }
+    }
+}
