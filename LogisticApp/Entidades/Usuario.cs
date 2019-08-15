1152c8bd91a224c64595329bbc338c0f1e844310
Elisabeth Campuzano J <elizacampu@gmail.com>
2019-08-15 00:17:13 -0500

    implementa usuarios

--- LogisticApp/Entidades/Usuario.cs
+++ LogisticApp/Entidades/Usuario.cs
@@ -7,5 +7,58 @@ namespace LogisticApp.Entidades
 {
     public class Usuario
     {
+        [Key]
+        public string Codigo { get; private set; }
+        public string Documento { get; private set; }
+        public string Contraseña { get; set; }
+        public Enum Perfil { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public bool Activo { get; set; }
+
+        private Usuario()
+        {
+
+        }
+
+        public static IEnumerable<Usuario> getUsuarios()
+        {
+
+        }
+
+        public static Usuario GetUsuario(string Codigo)
+        {
+            
+        }
+
+        public static void addUsuario(Usuario usuario)
+        {
+
+        }
+
+        public static void actualizarUsuario(Usuario usuario)
+        {
+
+        }
+
+        public static void modificarUsuario(Usuario usuario)
+        {
+
+        }
+
+        public static void desactivarUsuario(string CodigoUsuario)
+        {
+            
+        }
+
+        public void desactivarUsuario()
+        {
+            Activo = false;
+        }
+
+
     }
 }
