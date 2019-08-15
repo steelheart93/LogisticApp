b9e2fafd5ffa0146918d879c09e0e49a12aed185
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2019-08-14 21:49:52 -0500

    Agregando atributos a entradas

--- LogisticApp/Entidades/EntradaLote.cs
+++ LogisticApp/Entidades/EntradaLote.cs
@@ -7,5 +7,13 @@ namespace LogisticApp.Entidades
 {
     public class EntradaLote
     {
+        private string codigo { get; set; }
+        private int cantidadInicial { get; set; }
+        private int cantidadActual { get; set; }
+        private string origen { get; set; }
+        private DateTime fechaExpiracion { get; set; }
+        private DateTime fechaHoraRegistro { get; set; }
+        private string ubicacion { get; set; }
+        private string observaciones { get; set; }
     }
 }
