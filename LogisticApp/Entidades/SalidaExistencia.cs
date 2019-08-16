4803d9951eafab61381b0fc7c703f555e1d97074
Juan Sánchez <juan47s@hotmail.com>
2019-08-15 19:12:14 -0500

    entidad salida agregado key y private set

--- LogisticApp/Entidades/SalidaExistencia.cs
+++ LogisticApp/Entidades/SalidaExistencia.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace LogisticApp.Entidades
 {
     public class SalidaExistencia
     {
-        public string codigo { get; }
-        public DateTime fechaHoraRegistro { get; }
-        public string destino { get; }
-        public string observaciones { get; }
+        [key]
+        public string codigo { get; private set; }
+        public DateTime fechaHoraRegistro { get; private set; }
+        public string destino { get; private set; }
+        public string observaciones { get; private set; }
 
-        public Dictionary<EntradaLote, int> lotesSalida { get; }
-        public Usuario registrador { get; }
+        public Dictionary<EntradaLote, int> lotesSalida { get; private set; }
+        public Usuario registrador { get; private set; }
 
         public int getCantidadSalida()
         {
