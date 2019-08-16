208820139f8cb0aa418f9a828f4bf077283c2682
jlopezg8 <jlopezg8@gmail.com>
2019-08-15 20:12:58 -0500

    Revisión de ControladoraUsuarios, todo bien.

--- LogisticApp/ControladorasNegocio/ControladoraUsuarios.cs
+++ LogisticApp/ControladorasNegocio/ControladoraUsuarios.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LogisticApp.Entidades;
 
 namespace LogisticApp.ControladorasNegocio
 {
@@ -12,32 +13,32 @@ namespace LogisticApp.ControladorasNegocio
 
         }
 
-        public IEnumerable<Usuario> getUsuarios()
+        public static IEnumerable<Usuario> getUsuarios()
         {
-            return Usuario.getUsuarios();
+            return Usuario.getUsuarios(); //chulito
         }
 
         public Usuario GetUsuario(string Codigo)
         {
-            if (Usuario.getUsuario(Codigo) == null)
-            {
-                throw new Excepciones.UsuarioNoExisteException(Codigo);
-            }
-            return Usuario.getUsuario(codigo);
+            return Usuario.GetUsuario(Codigo);
         }
 
         public void addUsuario(Usuario usuario)
         {
-            if (Usuario.GetUsuario(usuario.GetCodigo()) != null)
-            {
-                throw new Excepciones.UsuarioYaExisteException(usuario.GetCodigo());
-            }
             Usuario.addUsuario(usuario);
         }
 
         public IEnumerable<Usuario> filtrarUsuarios(string datoUsuario)
         {
-            Usuario.filtrarUsuario(datoUsuario);
+            //Usuario.filtrarUsuario(datoUsuario);
+            string dato = datoUsuario.ToLower();
+            return from usuario in Usuario.getUsuarios()
+                   where usuario.Codigo.Contains(dato)
+                    || usuario.Documento.Contains(dato)
+                    || usuario.Nombres.ToLower().Contains(dato)
+                    || usuario.Apellidos.ToLower().Contains(dato)
+                   select usuario;
+                   
         }
 
         public void modificarUsuario(Usuario usuario)
