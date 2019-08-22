66950b4d538fd71b33be9a893f6b73c3ca8a9577
Juan Sánchez <juan47s@hotmail.com>
2019-08-22 18:43:08 -0500

    (pablo) terminada controladoraSalidas v1

--- LogisticApp/ControladorasNegocio/ControladoraSalidas.cs
+++ LogisticApp/ControladorasNegocio/ControladoraSalidas.cs
@@ -25,5 +25,36 @@ namespace LogisticApp.ControladorasNegocio
             }
             return listaSalidas;
         }
+
+        public void addSalida(string codigoProducto, SalidaExistencia salida)
+        {
+            Producto p = Producto.getProducto(codigoProducto);
+            p.addSalidaExistencia(salida);
+        }
+
+        public IEnumerable<EntradaLote> getLotesProducto(string codigoProducto)
+        {
+            Producto p = Producto.getProducto(codigoProducto);
+            return p.getEntradasLotes();
+        }
+
+        public IEnumerable<SalidaDetallada> filtrarSalidas(string datoSalida)
+        {
+            IEnumerable<Producto> productos = Producto.getProductos();
+            IEnumerable<SalidaExistencia> salidas;
+            List<SalidaDetallada> listaSalidas = new List<SalidaDetallada> { };
+            foreach (Producto p in productos)
+            {
+                salidas = p.getSalidasExistencias();
+                foreach (SalidaExistencia s in salidas)
+                {
+                    if (s.codigo == datoSalida || p.nombre == datoSalida || p.codigo == datoSalida)
+                    {
+                        listaSalidas.Add(new SalidaDetallada(s.codigo, s.fechaHoraRegistro, s.destino, s.observaciones, p.codigo, p.nombre, s.lotesSalida, s.registrador));
+                    }                    
+                }
+            }
+            return listaSalidas;
+        }
     }
 }
